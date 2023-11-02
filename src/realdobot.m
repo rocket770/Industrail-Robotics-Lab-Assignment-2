7e6e1f26b2136ab360406e7b01010a00e6a9c7a6
aidenadam7 <134464039+aidenadam7@users.noreply.github.com>
2023-11-02 17:06:09 +1100

    Update realdobot.m

--- src/realdobot.m
+++ src/realdobot.m
@@ -1,21 +1,26 @@
 %rosinit('192.168.27.1')
-
+%number of points on the circular path
 steps = 20;
-
+%arrays of zeros with a length of 'steps'
 x = zeros(1,steps);
 y = zeros(1,steps);
 
+%representing the radius of the circular path
 radius = 0.05;
 
+%distributing the steps to make up a full circle by evenly dividing it
 anlges = deg2rad(linspace(0,360,steps));
 
+%center of the circular path
 center = [0.2, 0, 0];
 
+%trig is used to create the circular path, the calculated x and y are centered around a point
 for i = 1:steps
     x(i) = cos(anlges(i)) * radius + center(1);
     y(i) = sin(anlges(i)) * radius + center(2);
 
 end
+
 
 % plot(x,y)
 
@@ -24,6 +29,7 @@ end
 %Get feedback about where the end effector pose is currently
 % endEffectorSub = rossubscriber('/dobot_magician/current_end_effector_pose');   
 
+%initial end effector position - no rotation 
 endEffectorRotation = [0,0,0];
 % qua = eul2quat(endEffectorRotation);
 
